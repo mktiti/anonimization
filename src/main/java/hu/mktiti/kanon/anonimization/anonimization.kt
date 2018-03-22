610ed144b1cf48bf35385e0108a33a02f6c9725f
titi <markuskrisztian1@gmail.com>
2018-03-22 23:02:15 +0100

    Comments added

--- src/main/java/hu/mktiti/kanon/anonimization/anonimization.kt
+++ src/main/java/hu/mktiti/kanon/anonimization/anonimization.kt
@@ -7,6 +7,9 @@ import hu.mktiti.kanon.attribute.RecordDescriptor
 import hu.mktiti.kanon.logger
 import java.util.logging.Level
 
+/**
+ * Data anonimization algoritm (strategy pattern)
+ */
 interface FullDataAnonimizationAlgorithm {
 
     fun anonimize()
@@ -16,13 +19,32 @@ interface FullDataAnonimizationAlgorithm {
 typealias Record = List<Any>
 typealias Data = List<Record>
 
+/**
+ * Anonimization core singleton engine
+ * Applies algorithms and gives information of the data.
+ */
 object AnonimizationEngine {
 
     private val log by logger()
 
+    /**
+     * Calculate k-anonimity of the data
+     *
+     * @param descriptor the data descriptor
+     * @param data the data
+     * @return k-anonimity of the data
+     */
     fun calculateKAnonimity(descriptor: RecordDescriptor, data: List<Record>): Int =
         splitToEqClasses(descriptor, data).map(List<Record>::size).min() ?: 0
 
+
+    /**
+     * Split to equality classes
+     *
+     * @param descriptor the data descriptor
+     * @param data the data
+     * @return blocks representing equality classes
+     */
     fun splitToEqClasses(descriptor: RecordDescriptor, data: List<Record>): List<Data> {
         val quasiIndexes = descriptor.attributes
                                 .mapIndexed(::Pair)
@@ -50,6 +72,15 @@ object AnonimizationEngine {
         return currentBlocks
     }
 
+    /**
+     * Split an equality class further by a previously unused quasi identifier attribute
+     *
+     * @param T the type of the column on which to base the splitting
+     * @param data a record block which represents an equality class in regards to previous quasi identifiers
+     * @param attribIndex index of the used column
+     * @param attribType type of the used column
+     * @return a mapping of the different values of the used column to their respective equality classes
+     */
     private fun <T : AttributeValue> splitByColumn(data: Data, attribIndex: Int, attribType: AttributeType<T>): Map<T, Data> {
         val result = HashMap<T, MutableList<Record>>()
 
